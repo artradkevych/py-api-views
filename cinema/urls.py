d600e129e92fe4d7b6419dd934ae32e4f67e4764
Artem <artemradckevich@gmail.com>
2026-05-17 20:32:33 +0300

    Fixed flake8 issues

--- cinema/urls.py
+++ cinema/urls.py
@@ -12,17 +12,17 @@ from cinema.views import (
 
 cinema_hall_list = CinemaHallViewSet.as_view(
     actions={
-           "get": "list",
-           "post": "create",
-       }
+        "get": "list",
+        "post": "create",
+    }
 )
 cinema_hall_detail = CinemaHallViewSet.as_view(
     actions={
-           "get": "retrieve",
-           "put": "update",
-           "patch": "partial_update",
-           "delete": "destroy",
-       }
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy",
+    }
 )
 
 router = routers.DefaultRouter()
